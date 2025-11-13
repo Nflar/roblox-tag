f33b1d4a66650d096300d11c2e2496f2d5a05b2a
Nflar <124061317+Nflar@users.noreply.github.com>
2025-11-13 19:47:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Install dependencies
-pip install -r requirements.txt
-
-# Start FastAPI server on port 8000
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn verify_api:app --host 0.0.0.0 --port $PORT
